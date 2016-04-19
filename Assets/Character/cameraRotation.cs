bff85381e19e94baaa45a65adbbe7096a41c1d3e
emileNetter <emile@netter.fr>
2016-04-19 17:14:19 +0200

    Premier commit

---
+++ Assets/Character/cameraRotation.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraRotation : MonoBehaviour
+{
+
+    private bool isRotatingDown;
+    private bool isRotatingUp; // Is the camera being rotated?
+
+    //
+    // UPDATE
+    //
+
+    void Update()
+    {
+        // On descend la caméra
+        if (Input.GetKeyDown("b"))
+        {
+            isRotatingDown = true;
+        }
+
+        // On remonte la caméra
+        if (Input.GetKeyDown("h"))
+        {
+            isRotatingUp = true;
+        }
+
+
+        // Rotate camera along X axis
+        if (isRotatingDown)
+        {
+            Vector3 pos = Camera.main.ScreenToViewportPoint(new Vector3(0, -280, 0));
+            float x = Camera.main.transform.eulerAngles.x;
+            if (x < 80)
+            {
+                transform.RotateAround(transform.position, transform.right, -pos.y);
+            }
+            else
+            {
+                isRotatingDown = false;
+            }
+        }
+
+        //Rotate camera along X axis
+        if (isRotatingUp)
+        {
+            Vector3 pos = Camera.main.ScreenToViewportPoint(new Vector3(0, -280, 0));
+            float x = Camera.main.transform.eulerAngles.x;
+            if (x > 2)
+            {
+                transform.RotateAround(transform.position, transform.right, pos.y);
+            }
+            else
+            {
+                isRotatingUp = false;
+            }
+        }
+    }
+    
+    //public void cameraUp()
+    //{
+    //    isRotatingUp = true;
+    //}
+}
---
+++ Assets/ManControls.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ManControls : MonoBehaviour {
+    private Animator myAnimator;
+    private float movementSpeed  = 5.0F;
+    private float rotationSpeed = 100F;
+    private Transform myTransform;
+
+	// Use this for initialization
+	void Start () {
+        myTransform = this.transform;
+        myAnimator = GetComponent<Animator>();
+       
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        // On place le manequin en position debout avant bras avant
+        if (Input.GetKeyDown("b"))
+        {
+            myAnimator.SetTrigger("isCarrying");
+        }
+
+        // On remet le manequin en position nomrale
+        if (Input.GetKeyDown("h"))
+        {
+            myAnimator.ResetTrigger("isCarrying");
+        }
+
+        if (Input.GetKeyDown("s"))
+        {
+            if (myAnimator.GetBool("isSit"))
+                myAnimator.ResetTrigger("isSit");
+            else
+                myAnimator.SetTrigger("isSit");
+        }
+
+        if (Input.GetKeyDown("t"))
+        {
+            if (myAnimator.GetBool("isDancing"))
+                myAnimator.ResetTrigger("isDancing");
+            else
+                myAnimator.SetTrigger("isDancing");
+        }
+
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        myAnimator.SetFloat("VSpeed", Input.GetAxis("Vertical"));
+        rotation *= Time.deltaTime;
+        transform.Rotate(0, rotation, 0);
+        
+
+
+    }
+    
+
+}
