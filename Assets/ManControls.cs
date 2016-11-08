528baeb7a09adc308d782e6dab7b74b8ba2bee80
emileNetter <emile@netter.fr>
2016-11-08 20:02:34 +0100

    Mouse moves head (some bugs)

--- Assets/ManControls.cs
+++ Assets/ManControls.cs
@@ -6,9 +6,14 @@ public class ManControls : MonoBehaviour {
     private float movementSpeed  = 5.0F;
     private float rotationSpeed = 100F;
     private Transform myTransform;
+    private float rightLeftRotation = 0.0f;
+    public float mouseSensitivity = 5.0f;
+    public float upDownRange = 60.0f;
+    float verticalRotation = 0;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
+
         myTransform = this.transform;
         myAnimator = GetComponent<Animator>();
        
@@ -46,14 +51,30 @@ public class ManControls : MonoBehaviour {
                 myAnimator.SetTrigger("isDancing");
         }
 
-        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
-        myAnimator.SetFloat("VSpeed", Input.GetAxis("Vertical"));
-        rotation *= Time.deltaTime;
-        transform.Rotate(0, rotation, 0);
-        
+        //float rotation1 = Input.GetAxis("Horizontal") * rotationSpeed;
+        //myAnimator.SetFloat("VSpeed", Input.GetAxis("Vertical"));
+        //rotation1 *= Time.deltaTime;
+        ////transform.Rotate(0, rotation1, 0);
+
+        float rotLeftright = Input.GetAxis("Mouse X");
+        transform.Rotate(0, rotLeftright, 0);
+
+        verticalRotation -= Input.GetAxis("Mouse Y");
+        verticalRotation = Mathf.Clamp(verticalRotation, -upDownRange, upDownRange);
+        Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+
+
+        float forwardSpeed = Input.GetAxis("Vertical") * movementSpeed;
+        float sideSpeed = Input.GetAxis("Horizontal") * movementSpeed;
+
+        Vector3 speed = new Vector3(sideSpeed, 0, forwardSpeed);
+        speed = transform.rotation * speed;
+        CharacterController cc = GetComponent<CharacterController>();
+        cc.SimpleMove(speed);
+
 
 
     }
-    
+
 
 }
